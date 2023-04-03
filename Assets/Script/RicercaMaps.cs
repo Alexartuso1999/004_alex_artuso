f7d833f35851c0e3fd21859a608267f46d85ce97
Alexartuso1999 <alexartuso1999@gmail.com>
2023-04-03 17:32:33 +0200

    altre cose

---
+++ Assets/Script/RicercaMaps.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class RicercaMaps : MonoBehaviour
+{
+    public TMP_InputField inputUser;
+    public GameObject map;
+    public GameObject disactive;
+    
+    public void ControllaTest()
+    {
+        inputUser.Select();
+        string text = inputUser.text;
+        if (text == "Cluedo city")
+        {
+            disactive.SetActive(false);
+            map.SetActive(true);
+        }
+    }
+}
