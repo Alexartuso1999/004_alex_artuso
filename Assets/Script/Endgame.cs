136a81a3c29540fc85b773255aa9804ef2443f4e
Alexartuso1999 <alexartuso1999@gmail.com>
2023-04-04 22:47:52 +0200

    altre cose

---
+++ Assets/Script/Endgame.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Endgame : MonoBehaviour
+{
+    public TMP_InputField inputUser;
+    public GameObject endgame;
+
+    public void ControllaTest()
+    {
+        inputUser.Select();
+        string text = inputUser.text;
+        if (text == "Z-99, Z-99, Z-99")
+        {
+            endgame.SetActive(true);
+        }
+    }
+}
