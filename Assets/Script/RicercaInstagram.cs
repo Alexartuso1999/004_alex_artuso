456d32d04159bdf1ad331d28d5a5ee7d92bb2541
Alexartuso1999 <alexartuso1999@gmail.com>
2023-04-04 11:32:10 +0200

    tante cose

---
+++ Assets/Script/RicercaInstagram.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class RicercaInstagram : MonoBehaviour
+{
+    public TMP_InputField inputUser;
+    public GameObject msScarlett;
+    public GameObject revGreen;
+    public GameObject plum;
+    public GameObject disactive;
+
+    public void ControllaTest()
+    {
+        inputUser.Select();
+        string text = inputUser.text;
+        if (text == "Ms Scarlett")
+        {
+            disactive.SetActive(false);
+            msScarlett.SetActive(true);
+        }
+        else if (text == "Rev Green")
+        {
+            disactive.SetActive(false);
+            revGreen.SetActive(true);
+        }
+        else if (text == "Plum")
+        {
+            disactive.SetActive(false);
+            plum.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Script/offGame.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class offGame : MonoBehaviour
+{
+    public void OffGame()
+    {
+        Application.Quit();
+    }
+}
